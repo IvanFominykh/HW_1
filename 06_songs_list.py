f0435c93da0a6a4a2b58ffe6f4f57b5038e4e84d
Fominykh Ivan <fominuh1990prof@yandex.ru>
2020-12-14 16:58:14 +0500

    7th exercise Done!

--- 06_songs_list.py
+++ 06_songs_list.py
@@ -20,7 +20,13 @@ violator_songs_list = [
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
-# TODO здесь ваш код
+# РЕШЕНИЕ
+
+time_halo = violator_songs_list[3][1]
+time_enjoy = violator_songs_list[5][1]
+time_clean =  violator_songs_list[-1][1]
+time_3songs = time_halo+time_enjoy+time_clean
+print ('Три песни звучат', round(time_3songs,2), 'мин')
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -38,4 +44,19 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+# РЕШЕНИЕ
+song_1=violator_songs_dict['World in My Eyes']
+song_2=violator_songs_dict['Sweetest Perfection']
+song_3=violator_songs_dict['Personal Jesus']
+song_4=violator_songs_dict['Halo']
+song_5=violator_songs_dict['Waiting for the Night']
+song_6=violator_songs_dict['Enjoy the Silence']
+song_7=violator_songs_dict['Policy of Truth']
+song_8=violator_songs_dict['Blue Dress']
+song_9=violator_songs_dict['Clean']
+
+time_3songs = song_2+song_7+song_8
+other_songs = song_1+song_3+song_4+song_5+song_6+song_9
+print ("Общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'", round(time_3songs,2), "минут")
+print ("Общее время звучания остальных шести песен:", round(other_songs,2), "минут")
+
