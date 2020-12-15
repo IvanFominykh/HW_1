d66f4ddc622fc187eec9c0245eab5303897df845
Fominykh Ivan <fominuh1990prof@yandex.ru>
2020-12-15 17:05:28 +0500

    8th exercise Done!

--- 07_secret.py
+++ 07_secret.py
@@ -21,4 +21,15 @@ secret_message = [
 # Требуется задать конкретные индексы, например secret_message[3][12:23:4]
 # Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
 
-# TODO вывести расшифрованное сообщение
+# РЕШЕНИЕ
+
+first_word = secret_message[0][3]
+second_word = secret_message[1][9:13]
+third_word = secret_message[2][5:14:2]
+fourth_word_raw = secret_message[3][7:13:] #Почему то не получилось сразу взять фрагмент в обратном порядке
+fourth_word = fourth_word_raw[::-1]
+fifth_word_raw = secret_message[4][16:21] #Почему то не получилось сразу взять фрагмент в обратном порядке
+fifht_word = fifth_word_raw[::-1]
+
+print(first_word.upper(), second_word, third_word, fourth_word, fifht_word, sep = ' ')
+
